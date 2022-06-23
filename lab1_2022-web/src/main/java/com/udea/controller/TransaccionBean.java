6e7ba5f2ef0b3635192c53d2b6677b130bdcc067
cbasguti <sebastian.gutierrezj@udea.edu.co>
2022-06-23 14:50:00 -0500

    Creación de JSF Managed Bean
    Controlador para la vista

---
+++ lab1_2022-web/src/main/java/com/udea/controller/TransaccionBean.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.udea.controller;
+
+/**
+ *
+ * @author sebas
+ */
+public class TransaccionBean {
+
+    /**
+     * Creates a new instance of TransaccionBean
+     */
+    public TransaccionBean() {
+    }
+    
+}
